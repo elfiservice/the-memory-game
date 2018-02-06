c39937ffd0b56648acd745092b162b791ec72859
Armando Jr <elfiservice@hotmail.com>
2018-02-06 09:15:26 -0300

    feat: build the grid structure

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
 $(function() {
 
     let model = {
-
+        Grid: function (row, col) {
+            this.row = row,
+            this.col = col
+        }
     }
 
     let octupus = {
+        getGrid: (row, col) => {
+            return new model.Grid(row, col);
+        },
 
         init: () => {
-            alert('testing');
+            view.init();
         }
     }
 
     let view = {
-
+        init: () => {
+            //get instance for the Grid
+            let Grid = octupus.getGrid(4,4);
+            let gridElement = document.getElementById('game_grid');
+           
+            for(let r = 1; r <= Grid.row; r++) {
+                let createRow = document.createElement("tr");
+                for(let c = 1; c <= Grid.col; c++) {
+                    let createdCol = document.createElement("td");
+                    createdCol.innerText = r + "-" + c;
+                    createRow.appendChild(createdCol);
+                }
+                gridElement.appendChild(createRow);
+            }
+            
+        }
     }
 
     //app start here
