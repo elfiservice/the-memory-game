1c4029cf8212efa88b940a7eaff0c64efe063858
Armando Jr <elfiservice@hotmail.com>
2018-02-07 09:06:47 -0300

    feat: provided symbols to the cards

--- js/app.js
+++ js/app.js
@@ -4,12 +4,26 @@ $(function() {
         Grid: function (row, col) {
             this.row = row,
             this.col = col
+        },
+        level: function (levelNumber) {
+            switch(levelNumber) {
+                case 0: return ['☯','♫','☯','♫','♞','♞','☂','☂','☀','☀','❤','❤','😀','😀','😜','😜']; break;
+                default: console.log('Error Level not passed');
+                
+            }
         }
     }
 
     let octupus = {
         getGrid: (row, col) => {
             return new model.Grid(row, col);
+        },
+        getLevel: (level) => {
+            let cardsArray = model.level(level);
+            //Sorting an Array in Random Order
+            return cardsArray.sort(function(a, b){return 0.5 - Math.random()});
+     
+            
         },
 
         init: () => {
@@ -19,19 +33,42 @@ $(function() {
 
     let view = {
         init: () => {
+            //get a array with values for the level -> level 0 = 16 cards
+            let cardsArray = octupus.getLevel(0);
+            let  numeberOfCards = cardsArray.length;
+            //toDo: find the best algoritm to divide correct distribuition for the memory game
+            const numRows = Math.round(numeberOfCards/4);
+            const numCol = numRows;
+            
+            
             //get instance for the Grid
-            let Grid = octupus.getGrid(4,4);
+            let Grid = octupus.getGrid(numRows, numCol);
             let gridElement = document.getElementById('game_grid');
-           
+            //improved the performance of 0.312ms to 0.195ms using the fragment
+            let fragment = document.createDocumentFragment();
+            let numberCardsCount = 0;
             for(let r = 1; r <= Grid.row; r++) {
                 let createRow = document.createElement("tr");
                 for(let c = 1; c <= Grid.col; c++) {
                     let createdCol = document.createElement("td");
-                    createdCol.innerText = r + "-" + c;
+                    createdCol.innerText = cardsArray[numberCardsCount];
                     createRow.appendChild(createdCol);
+                    numberCardsCount++;
                 }
-                gridElement.appendChild(createRow);
+                
+                fragment.appendChild(createRow);
+                
             }
+            gridElement.appendChild(fragment);
+
+            
+
+            // let t0 =  performance.now();
+
+        //     let t1 =  performance.now();
+        //  console.log(t1 - t0);
+         
+
             
         }
     }
